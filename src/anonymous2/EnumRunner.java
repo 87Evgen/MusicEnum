9f5ec95b9916b052546581fa24113789167a6475
87Evgen <pro.scorp@gmail.com>
2019-04-10 17:47:40 +0300

    enum_anonumous

---
+++ src/anonymous2/EnumRunner.java
@@ -0,0 +1,11 @@
+package anonymous2;
+
+public class EnumRunner {
+    public static void main(String[] args) {
+        int i = 4;
+        for (Shape f : Shape.values()) {
+            f.setShape(3, i--);
+            System.out.println(f + " площадь= " + f.computeSquare());
+        }
+    }
+}
